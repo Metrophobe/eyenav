4337240c2c74a510dc4122dd6b99319e56fbc870
Metrophobe <metrophobe@outlook.com>
2023-11-03 00:00:41 +0100

    patched calib issue

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,32 @@
 
 'use strict'
 
-import { webgazer } from "./webgazer.js";
-
-
-
 let highlighted = 0;
 let total = 98;
 let preview = false;
+let pointer;
+let container;
+let canCallibrate = false;
 
 
-let init = () => {
+let setupScreen = () => {
+    container = document.getElementsByClassName("container")[0];
+    pointer = document.getElementById("pointer");
+    for (let x = 1; x <= total; x++) {
+        let tmp = document.createElement("div");
+        tmp.classList.add("box");
+        tmp.innerText = x;
+        container.append(tmp);
+    }
+    document.body.addEventListener("click", (evt) => {
+        if(canCallibrate){
+            webgazer.addCallibrationPont(evt.x,evt.y);
+        }
+    });
+}
+
+
+let track = () => {
     window.saveDataAcrossSessions = true;
     webgazer.setTracker("TFFacemesh");
     webgazer.showVideoPreview(preview);
@@ -18,31 +34,27 @@ let init = () => {
         if (data == null) {
             return;
         }
-        let selected = document.elementFromPoint(data.x,data.y);
-        if(selected!= undefined && selected.className == "box"){
-            if(!selected.classList.contains("selected")){
+        let selected = document.elementFromPoint(data.x, data.y);
+        pointer.style.top = data.y + "px";
+        pointer.style.left = data.x + "px";;
+        if (selected != undefined && selected.className == "box") {
+            if (!selected.classList.contains("selected")) {
                 selected.classList.add("selected");
                 highlighted++;
             }
         }
-        if(highlighted==total){
+        if (highlighted == total) {
             document.querySelectorAll(".box").forEach(e => e.classList.remove("selected"));
             highlighted = 0;
         }
-    }).begin();
-
+    }
+    ).begin();
+    canCallibrate = true;
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    //init();
-    let container = document.getElementsByClassName("container")[0];
-    init();
-    for (let x = 1; x <= total; x++) {
-        let tmp = document.createElement("div");
-        tmp.classList.add("box");
-        tmp.innerText = x;
-        container.append(tmp);
-    }
+    setupScreen();
+    track();
 })
 
